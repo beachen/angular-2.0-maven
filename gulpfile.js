a432dc3f86c55852377ba46a0d57dce58b5b1705
Anders Strand <andersstrand.se@gmail.com>
2016-11-03 07:23:39 +0100

    Added gulpfile for Angular2 dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -9,39 +9,27 @@ gulp.task('clean', function () {
 // Copy dependencies to libs
 gulp.task('copy:libs', ['clean'], function() {
 	
-	console.log("This is the gulp phase and reallly cools stuff can be made..");
+	console.log("Copy js files");
 	
 	gulp.src([
       'node_modules/core-js/client/shim.min.js',
       'node_modules/zone.js/dist/zone.js',
 	  'node_modules/reflect-metadata/Reflect.js',
 	  'node_modules/systemjs/dist/system.src.js'
-//	  ,
-//	  'node_modules/@angular/**/*.js',
-//	  'node_modules/rxjs/**/*.js'
-	  
-	   // Bundles
-//	  'node_modules/@angular/core/bundles/core.umd.js',
-//	  'node_modules/@angular/common/bundles/common.umd.js',
-//      'node_modules/@angular/compiler/bundles/compiler.umd.js',
-//      'node_modules/@angular/platform-browser/bundles/platform-browser.umd.js',
-//      'node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-//      'node_modules/@angular/http/bundles/http.umd.js',
-//      'node_modules/@angular/router/bundles/router.umd.js',
-//      'node_modules/@angular/forms/bundles/forms.umd.js'
-	  
-	  
 	  
     ])
     .pipe(gulp.dest('lib'));
 	
 	// Bundels - just take the js files
+	console.log("Copy @angular bundles");
 	gulp.src('node_modules/@angular/**/*.js')
-	.pipe(gulp.dest('lib/@angular/'));  // Writes 'build/somedir/somefile.js'
+	.pipe(gulp.dest('lib/@angular/'));  
 
-	gulp.src('node_modules/rxjs/**/*.js')
-	.pipe(gulp.dest('lib/rxjs/'));  // Writes 'build/js/somedir/somefile.js'
-	
+	// rxjs
+	console.log("Copy rxjs bundles");
+	gulp.src(
+			'node_modules/rxjs/**/*.js')
+	.pipe(gulp.dest('lib/rxjs/'));  
 });
 
 
--- src/main/webapp/app/app.components.ts
+++ src/main/webapp/app/app.components.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular App</h1>'
+  template: '<h1>My First Angular App Build with Maven</h1><h2>Wow!</h2>'
 })
 export class AppComponent { }
